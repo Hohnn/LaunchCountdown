903d15806d1df12c50bc22e01fa2f53c4f16d188
Hohnn <jean-bateee@hotmail.fr>
2021-04-19 08:37:22 +0200

    bordel

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ function refresh() {
     setTimeout('showDate()', t)
 }
 
+
 function showDate() {
     let date = new Date();
     let d = date.getDate();
@@ -40,7 +41,7 @@ function showDate() {
     let hl = launch.getHours();
     let ml = launch.getMinutes();
     let sl = launch.getSeconds();
-    refresh();
+    /* refresh(); */
     /* console.log((h + (24*((dl-d)-1)))-hl)
     console.log(d - dl)
     console.log((23 - h) + hl)
@@ -69,22 +70,73 @@ function showDate() {
         secondsIn.innerHTML = '0'+ secondsIn.innerHTML
     }
 
-    /* cardFlip(); */
+    
 
+    const coltest = document.getElementById("coltest")
+    const colSec = document.getElementById("colSec")
+
+    /* addCard(); */
+    refresh();
+    setTimeout(addCard(), 1000)
+
+
+
+    function addCard() {
+        
+        const flipNumber = document.querySelectorAll('.sec')
+        let para = document.createElement("div")
+        para.classList.add("card", "haut", "sec")
+        para.innerHTML = '<div class="card_face card_front"><div class="num num1" id="frontnum">'+(secondesRest)+'</div></div><div class="card_face card_back"><div class="num num2" id="backnum">'+(secondesRest-1)+'</div></div>'
+
+        colSec.appendChild(para);
+        const cone = document.querySelectorAll('.sec')
+        cone[1].children[0].firstElementChild.textContent = secondesRest
+        console.log(cone)
+        
+            
+            for (let a = 0; a < flipNumber.length; a++) {               
+                
+                function addcardFlip() {
+                    if (cone[a].classList.contains('flip')) {
+                    } else {
+                        cone[a].classList.toggle('flip')
+                        if (cone.length > 3) {
+                        cone[a].parentElement.removeChild(cone[0])
+                        }
+                                                
+                    }
+                           
+                }       
+                addcardFlip();
+        
+        }
+
+    }
+
+    
 }
 
+       
+/* 
+function cardFlip() {
+    if (coltest.classList.contains('flip')) {
+        console.log("yes")
+    } else {
+        coltest.classList.toggle('flip')
+}
 
-/* const coltest = document.getElementById("coltest")
+} */
 
-coltest.addEventListener('click', () => {
+
+/* coltest.addEventListener('click', () => {
     coltest.classList.toggle('flip')
 }) */
 
-const card = document.querySelectorAll('.card')
+/* const card = document.querySelectorAll('.card') */
 
 
 
-for (let i = 0; i < card.length; i++) {
+/* for (let i = 0; i < card.length; i++) {
     card[i].addEventListener('click', () => {
         card[i].classList.toggle('flip')
         console.log(card[i])        
@@ -96,5 +148,5 @@ for (let i = 0; i < card.length; i++) {
         }
         
     }
-}
+} */
 
