6b57b38ba2e0d502508cecaabc293e31a095cb33
Hohnn <jean-bateee@hotmail.fr>
2021-04-21 19:38:21 +0200

    add minutes + clean + explication

--- script.js
+++ script.js
@@ -22,6 +22,8 @@ console.log(hours)
 let days = Math.floor(hours / 24)
 console.log(days) */
 
+
+
 function refresh() {
     let t = 1000;
     setTimeout('showDate()', t)
@@ -57,7 +59,7 @@ function showDate() {
     minutesIn.innerHTML = minutesRest
     secondsIn.innerHTML = secondesRest
 
-    let back = secondesRest-1
+
 
     if ( daysIn.innerHTML < 10 ) {
         daysIn.innerHTML = '0'+ daysIn.innerHTML
@@ -71,61 +73,107 @@ function showDate() {
     if ( secondsIn.innerHTML < 10 ) {
         secondsIn.innerHTML = '0'+ secondsIn.innerHTML
     }
-
-    if (back == 0) {
-        back = 60
+    let back = secondesRest-1
+    if (secondesRest < 10) {
+        secondesRest = '0'+secondesRest     
+    }
+    if (secondesRest < 11) {
+        back = '0'+(secondesRest-1)        
+    }
+    if (minutesRest < 10) {
+        minutesRest = '0'+minutesRest    
+    }
+    let backNum = minutesRest-1
+    if (minutesRest < 11) {
+        backNum = '0'+(minutesRest-1)        
     }
 
-    
     refresh();
+    const backnum = document.getElementById("backnum")    
+    const backnumMin = document.getElementById("backnumMin")
+    
+    
     
 
-    const coltest = document.getElementById("coltest")
-    const colSec = document.getElementById("colSec")
-    const flipNumber = document.querySelectorAll('.sec')
+    
     const cone = document.querySelectorAll('.sec')
     const cardAdd = document.getElementById("cardAdd")
+    const cardAddMin = document.getElementById("cardAddMin")
+    const coneMin = document.querySelectorAll('.min')
 
+
+    // affiche le numéro pour la premiere card
+    if (cardAdd.children.length === 1) {
+        backnum.innerHTML = secondesRest
+    };
+
+    if (cardAddMin.children.length === 1) {
+        backnumMin.innerHTML = minutesRest
+    };
+    //ajoute une card, si moins de 3 elements
+    function addCardMin() {  
+        let para = document.createElement("div")
+        para.innerHTML = '<div class="card haut min" id="coltest"><div class="card_face card_front"><div class="num num1" id="frontnum">'+minutesRest+'</div></div><div class="card_face card_back"><div class="num num2">'+backNum+'</div></div></div>'
+        if (cardAddMin.children.length < 3) {
+            cardAddMin.insertBefore(para, cardAddMin.lastElementChild)
+        }        
+    };
+    //si le nombre de card est plus de 4, supprime la first
+    if (coneMin.length > 4) {
+        cardAddMin.removeChild(cardAddMin.firstElementChild)      
+        };
+
+    for (let a = 0; a < coneMin.length; a++) {
+                  
+         function addcardFlipmin() {
+            //check si les card classe ont flip, pour lancer le reste qu'une fois 
+            if (coneMin[a].classList.contains('flip')) {
+                //si c'est 60 sec ou moins de 2 card ça ajoute et flip                
+            } else if ( secondesRest == 60 || cardAddMin.children.length < 2) {
+                //créer une div
+                let para = document.createElement("div")
+                para.innerHTML = '<div class="card haut min" id="coltest"><div class="card_face card_front"><div class="num num1" id="frontnum">'+minutesRest+'</div></div><div class="card_face card_back"><div class="num num2">'+backNum+'</div></div></div>'
+                //l'ajoute avant la derniere card pour ne pas la voir pop
+                cardAddMin.insertBefore(para, cardAddMin.lastElementChild)
+                console.log('what')
+                //la flip et change le z index pour l'afficher devant
+                coneMin[a].classList.add('flip')
+                //passe la card initiale derriere
+                cardAddMin.lastElementChild.classList.add('z')
+                
+            }; 
+            
+        };          
+           
+    };
+
+    addcardFlipmin();
+    addCardMin();
     addCard();
     
 
     function addCard() {  
     
         let para = document.createElement("div")
-        para.innerHTML = '<div class="card haut sec" id="coltest"><div class="card_face card_front"><div class="num num1" id="frontnum">'+secondesRest+'</div></div><div class="card_face card_back"><div class="num num2" id="backnum">'+back+'</div></div></div>'
-        //cardAdd.appendChild(para);
-        cardAdd.insertBefore(para, cardAdd.lastElementChild)    
-        //cone[1].children[0].firstElementChild.textContent = secondesRest
-        //console.log(cone)
+        para.innerHTML = '<div class="card haut sec" id="coltest"><div class="card_face card_front"><div class="num num1" id="frontnum">'+secondesRest+'</div></div><div class="card_face card_back"><div class="num num2">'+back+'</div></div></div>'
+        cardAdd.insertBefore(para, cardAdd.lastElementChild)   
+    
     }
+    if (cone.length > 3) {
+        cardAdd.removeChild(cardAdd.firstElementChild)
+        
+       
+        
+        }
+    for (let a = 0; a < cone.length; a++) {               
 
-    for (let a = 0; a < flipNumber.length; a++) {               
-        console.log(a)
-        //console.log(cone.length)
         function addcardFlip() {
-            if (cone[a].classList.contains('flip')) {
-            } else {
-                cone[a].classList.toggle('flip')
-                cone[a].classList.toggle('kk')
-                if (cone.length > 3) {
-                cardAdd.removeChild(cardAdd.firstElementChild)
-                cardAdd.lastElementChild.classList.add('z')
-                //setTimeout(cardAdd.insertBefore(cardAdd.lastElementChild, cardAdd.children[2]),1000)
-                
-                } 
-                                        
-            }
-            //cardAdd.insertBefore(cardAdd.lastElementChild, cardAdd.children[2])
-            //setTimeout(cardAdd.insertBefore(cardAdd.lastElementChild, cardAdd.children[2]),1000)
-            /* if (cardAdd.lastElementChild.classList.contains('gg')) {
-                cardAdd.removeChild(cardAdd.lastElementChild)
-                console.log('oui')
-            }  */
-                    
+            cone[a].classList.add('flip')
+            cardAdd.lastElementChild.classList.add('z')
+        
         }
           
         addcardFlip();    
-        
     }
 
     
